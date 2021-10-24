daba52a0a4361170d2bf04ee230bf1066d3242aa
Marek Vitvar <m.vitvar@gmail.com>
2021-10-24 02:00:57 +0200

    use current commit number as lib version

--- RPNCalc/Assembly.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyProduct("RPNCalc")]
-[assembly: AssemblyTitle("RPNCalc")]
-[assembly: AssemblyCopyright("Copyright (C) 2021 Marek Vitvar")]
-[assembly: AssemblyVersion("1.4.1.*")]
-[assembly: AssemblyFileVersion("1.4.1.0")]
-[assembly: AssemblyInformationalVersion("1.4.1")]
